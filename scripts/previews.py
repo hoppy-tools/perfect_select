418cf86188cfb2474aeada153ec6674118fa654b
Dawid Czech <hophead.ninja@gmail.com>
2022-04-30 18:36:29 +0200

    Previews code cleanup and moving

--- scripts/previews.py
+++ scripts/previews.py
@@ -3,6 +3,7 @@ import os
 import bpy
 import bgl
 import gpu
+import numpy
 
 from math import radians
 
@@ -145,6 +146,92 @@ def _get_render_result(scene, image):
 
 def get_active_color(alpha=1.0):
     return (*bpy.context.preferences.themes[0].view_3d.vertex_select, alpha)
+
+
+#
+# Properties functions
+#
+
+def _create_tmp_scene_and_render(context, obj, axis, resolution):
+    scene = bpy.data.scenes.new("_tmp_perfect_select")
+    original_scene = bpy.context.window.scene
+    context.window.scene = scene
+
+    scene.render.engine = "BLENDER_WORKBENCH"
+    scene.render.resolution_x = 512
+    scene.render.resolution_y = 512
+    scene.render.film_transparent = True
+    scene.display.render_aa = "OFF"
+
+    new_obj = obj.copy()
+    new_data = obj.data.copy()
+    new_obj.data = new_data
+    new_obj.location = (0.0, 0.0, 0.0)
+    new_obj.rotation_euler.zero()
+    new_obj.scale = (1.0, 1.0, 1.0)
+    scene.collection.objects.link(new_obj)
+
+    camera = bpy.data.cameras.new("_tmp_perfect_select_camera")
+    camera.type = 'ORTHO'
+    camera_obj = bpy.data.objects.new("_tmp_perfect_select_camera", camera)
+    camera_obj.location = (0.0, 0.0, 2.0)
+    scene.collection.objects.link(camera_obj)
+    scene.camera = camera_obj
+
+    pixels = render_preview(scene, new_obj, camera_obj, axis, resolution)
+
+    data_collections = {
+        "MESH": bpy.data.meshes,
+        "CURVE": bpy.data.curves,
+        "SURFACE": bpy.data.curves,
+        "FONT": bpy.data.curves,
+        "GPENCIL": bpy.data.grease_pencils
+    }
+
+    scene.collection.objects.unlink(camera_obj)
+    scene.collection.objects.unlink(new_obj)
+    bpy.data.objects.remove(camera_obj)
+    bpy.data.cameras.remove(camera)
+
+    data_collection = data_collections.get(new_obj.type)
+    bpy.data.objects.remove(new_obj)
+    data_collection.remove(new_data)
+
+    context.window.scene = original_scene
+    bpy.data.scenes.remove(scene)
+
+    return pixels
+
+
+def _create_from_image_and_render(context, image, resolution):
+    pixels = render_preview(None, image, None, None, resolution)
+    return pixels
+
+
+def update_preview(self, context):
+    if self.pattern_source == "OBJECT":
+        if self.pattern_data is None:
+            return
+        _create_tmp_scene_and_render(context, self.pattern_data, self.pattern_projection, self.pattern_resolution)
+    elif self.pattern_source == "IMAGE":
+        if self.pattern_data_image is None or not self.pattern_data_image.has_data:
+            return
+        _create_from_image_and_render(context, self.pattern_data_image, self.pattern_resolution)
+
+
+def update_preview_chess(self, context):
+    render_preview(resolution=self.pattern_resolution, only_chess=True)
+
+
+def get_selection_pattern_buffer(resolution):
+    preview = get_preview()
+    pixels = preview.image_pixels_float
+    pixels_buffer = numpy.asarray(pixels, dtype=numpy.bool)
+    pixels_buffer = pixels_buffer[3::4]
+    pixels_buffer = pixels_buffer.reshape((PREVIEW_WIDTH, PREVIEW_HEIGHT))
+    pattern_buffer = numpy.asarray([[pixels_buffer[PREVIEW_WIDTH * h // resolution][PREVIEW_HEIGHT * w // resolution]
+                                     for h in range(resolution)] for w in range(resolution)])
+    return pattern_buffer
 
 
 def register():
